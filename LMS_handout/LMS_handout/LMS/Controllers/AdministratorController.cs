2889c91e38d27956348555301a5799ed28cdc1a5
jhiggi44 <jordandhiggins@gmail.com>
2019-07-16 12:19:06 -0700

    create courses controller

--- LMS_handout/LMS_handout/LMS/Controllers/AdministratorController.cs
+++ LMS_handout/LMS_handout/LMS/Controllers/AdministratorController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using LMS.Models.LMSModels;
 
 namespace LMS.Controllers
 {
@@ -28,36 +29,34 @@ namespace LMS.Controllers
       return View();
     }
 
-    /*******Begin code to modify********/
+        /*******Begin code to modify********/
 
-    /// <summary>
-    /// Returns a JSON array of all the courses in the given department.
-    /// Each object in the array should have the following fields:
-    /// "number" - The course number (as in 5530)
-    /// "name" - The course name (as in "Database Systems")
-    /// </summary>
-    /// <param name="subject">The department subject abbreviation (as in "CS")</param>
-    /// <returns>The JSON result</returns>
-    public IActionResult GetCourses(string subject)
-    {
-      
-      return Json(null);
-    }
+        /// <summary>
+        /// Returns a JSON array of all the courses in the given department.
+        /// Each object in the array should have the following fields:
+        /// "number" - The course number (as in 5530)
+        /// "name" - The course name (as in "Database Systems")
+        /// </summary>
+        /// <param name="subject">The department subject abbreviation (as in "CS")</param>
+        /// <returns>The JSON result</returns>
+        public IActionResult GetCourses(string subject) {
+            return Json(null);
+        }
 
 
-    
 
 
-    /// <summary>
-    /// Returns a JSON array of all the professors working in a given department.
-    /// Each object in the array should have the following fields:
-    /// "lname" - The professor's last name
-    /// "fname" - The professor's first name
-    /// "uid" - The professor's uid
-    /// </summary>
-    /// <param name="subject">The department subject abbreviation</param>
-    /// <returns>The JSON result</returns>
-    public IActionResult GetProfessors(string subject)
+
+        /// <summary>
+        /// Returns a JSON array of all the professors working in a given department.
+        /// Each object in the array should have the following fields:
+        /// "lname" - The professor's last name
+        /// "fname" - The professor's first name
+        /// "uid" - The professor's uid
+        /// </summary>
+        /// <param name="subject">The department subject abbreviation</param>
+        /// <returns>The JSON result</returns>
+        public IActionResult GetProfessors(string subject)
     {
    
       return Json(null);
@@ -65,41 +64,51 @@ namespace LMS.Controllers
 
 
 
-    /// <summary>
-    /// Creates a course.
-    /// A course is uniquely identified by its number + the subject to which it belongs
-    /// </summary>
-    /// <param name="subject">The subject abbreviation for the department in which the course will be added</param>
-    /// <param name="number">The course number</param>
-    /// <param name="name">The course name</param>
-    /// <returns>A JSON object containing {success = true/false}.
-    /// false if the course already exists, true otherwise.</returns>
-    public IActionResult CreateCourse(string subject, int number, string name)
-    {
-      
+        /// <summary>
+        /// Creates a course.
+        /// A course is uniquely identified by its number + the subject to which it belongs
+        /// </summary>
+        /// <param name="subject">The subject abbreviation for the department in which the course will be added</param>
+        /// <param name="number">The course number</param>
+        /// <param name="name">The course name</param>
+        /// <returns>A JSON object containing {success = true/false}.
+        /// false if the course already exists, true otherwise.</returns>
+        public IActionResult CreateCourse(string subject, int number, string name) {
+            using (Team12LMSContext db = new Team12LMSContext()) {
+                Courses course = new Courses { Name = name, Number = (ushort)number, Department = subject };
 
-      return Json(new { success = false });
-    }
+                db.Courses.Add(course);
+
+                try {
+                    db.SaveChanges();
+                } catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                    return Json(new { success = false });
+                }
+
+                return Json(new { success = true });
+            } 
+        }
 
 
 
-    /// <summary>
-    /// Creates a class offering of a given course.
-    /// </summary>
-    /// <param name="subject">The department subject abbreviation</param>
-    /// <param name="number">The course number</param>
-    /// <param name="season">The season part of the semester</param>
-    /// <param name="year">The year part of the semester</param>
-    /// <param name="start">The start time</param>
-    /// <param name="end">The end time</param>
-    /// <param name="location">The location</param>
-    /// <param name="instructor">The uid of the professor</param>
-    /// <returns>A JSON object containing {success = true/false}. 
-    /// false if another class occupies the same location during any time 
-    /// within the start-end range in the same semester, or if there is already
-    /// a Class offering of the same Course in the same Semester,
-    /// true otherwise.</returns>
-    public IActionResult CreateClass(string subject, int number, string season, int year, DateTime start, DateTime end, string location, string instructor)
+        /// <summary>
+        /// Creates a class offering of a given course.
+        /// </summary>
+        /// <param name="subject">The department subject abbreviation</param>
+        /// <param name="number">The course number</param>
+        /// <param name="season">The season part of the semester</param>
+        /// <param name="year">The year part of the semester</param>
+        /// <param name="start">The start time</param>
+        /// <param name="end">The end time</param>
+        /// <param name="location">The location</param>
+        /// <param name="instructor">The uid of the professor</param>
+        /// <returns>A JSON object containing {success = true/false}. 
+        /// false if another class occupies the same location during any time 
+        /// within the start-end range in the same semester, or if there is already
+        /// a Class offering of the same Course in the same Semester,
+        /// true otherwise.</returns>
+        public IActionResult CreateClass(string subject, int number, string season, int year, DateTime start, DateTime end, string location, string instructor)
     {
       
       return Json(new { success = false });
